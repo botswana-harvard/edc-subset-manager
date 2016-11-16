db17b5181980b8effe9917db8083a7b5f5c7a724
erikvw <ew2789@gmail.com>
2016-11-16 16:15:12 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='edc-subset-manager',
+    version='0.1.0dev0',
+    author=u'Erik van Widenfelt',
+    author_email='ew2789@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='http://github/erikvw/edc-subset-manager',
+    license='GPL licence, see LICENCE',
+    description='A custom manager to limit get_queryset to a subset of records based on a relation.',
+    long_description=README,
+    zip_safe=False,
+    keywords='django get_queryset manager filter limit',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
